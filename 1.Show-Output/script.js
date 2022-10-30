27b1617858657aeb0f5097ab6a4ba23e240a1640
rakibul667s <rakibulislam667s@gmail.com>
2022-10-30 14:37:23 +0600

    first commit

---
+++ 1.Show-Output/script.js
@@ -0,0 +1,17 @@
+// Writing into an HTML element, using innerHTML.
+// Writing into the HTML output using document.write().
+// Writing into an alert box, using window.alert().
+// Writing into the browser console, using console.log().
+
+// Output show in "alert" funtion
+alert("Show-Output");
+
+// Output show in Document.write
+//document.write("Document Output"); 
+
+// Output show in console.log
+console.log("Console Output");
+
+const data = document.getElementById("demo");
+data.innerHTML=5+6;
+ 
