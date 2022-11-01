c5356cff250f9aa4ff2c94264fd4471c079afef0
rakibul667s <rakibulislam667s@gmail.com>
2022-11-01 14:08:44 +0600

    numberMethod

---
+++ number-method/main.js
@@ -0,0 +1,12 @@
+//number data type
+var num = 20;
+console.log(typeof(num));
+
+//String data type
+var num = "20";
+console.log(typeof(num));
+
+//Number to String convert
+var num2 = 30;
+num2 = toString(num2);
+console.log(typeof(num2));
