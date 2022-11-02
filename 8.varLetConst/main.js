5b9e7942e3a8f328bec155313837d8c611da17f2
rakibul667s <rakibulislam667s@gmail.com>
2022-11-02 11:42:43 +0600

    varLetConst

---
+++ 8.varLetConst/main.js
@@ -0,0 +1,20 @@
+//var 
+console.log("var output");
+var x =10;
+console.log(x);
+if(true){
+  x=5;
+  console.log(x);
+}
+console.log(x);
+
+
+//let
+console.log("let output");
+let y = 10;
+console.log(y);
+if(true){
+  let y=5;
+  console.log(y);
+}
+console.log(y);
