02dbf5870fbc5e1b1951ba458f73631e61448e74
rakibul667s <rakibulislam667s@gmail.com>
2022-11-02 21:45:29 +0600

    object

---
+++ 10.object/main.js
@@ -0,0 +1,48 @@
+const car ={
+    name:"audi",
+    model:"110",
+    weight:"567kg",
+    color:"white",
+    price:10,
+    totalPrice:10,
+
+    start:function(){
+        console.log("Car has Started...");
+        
+    },
+
+    drive:function(){
+        console.log("Car is driving...");
+        this.start();
+    },
+
+    properties:function(model,weight,color,price){
+        this.drive();
+        this.weight = weight;  
+        this.color = color;
+        this.price = price;
+        this.totalPrice = this.vat(price);
+        this.display(this.name,this.model=model,this.weight,this.color,this.price,this.totalPrice);
+    },
+
+    vat:function(price){
+        return price+((price/100)*10);
+    },
+
+    display:function(name,model,weight,color,price,totalPrice){
+        const demo = document.getElementById("demo");
+        demo.innerHTML = `
+        Car name: ${name} <br>
+        Model: ${model} <br>
+        Weight: ${weight}<br>
+        Color: ${color}<br>
+        Price: ${price}$<br> 
+        Vat: 10% <br>
+        TotalPrice: ${totalPrice}$<br>
+        `
+    },
+};
+
+car.properties("101","680kg","White",3400);
+
+
