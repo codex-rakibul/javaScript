eb8522f3b06304b8e7bcec4151c39b103cb0d755
rakibul667s <rakibulislam667s@gmail.com>
2022-11-06 15:42:54 +0600

    forOf

---
+++ 18.forOf/main.js
@@ -0,0 +1,14 @@
+//for_Of_Loop best for array
+
+let language = "JavaScript";
+let text = "";
+for (let x of language) {
+console.log(x);
+}
+
+
+const cars = ["BMW", "Volvo", "Mini"];
+for (let x of cars) {
+  console.log(x);
+}
+
