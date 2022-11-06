541eb8cabb586fe2393cb486ca574f44df69e362
rakibul667s <rakibulislam667s@gmail.com>
2022-11-06 16:00:30 +0600

    whileLoop

---
+++ 19.whileLoop/main.js
@@ -0,0 +1,19 @@
+//while loop
+console.log("while loop");
+let x=10;
+let i=0;
+
+while(i<x){
+    console.log(i);
+    i++;
+}
+
+//do while
+console.log(" do while loop");
+let a=10;
+let j=0;
+do{
+    console.log("The number"+j);
+    j++;
+
+}while(j<=a);
