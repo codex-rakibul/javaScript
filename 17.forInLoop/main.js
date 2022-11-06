2e02cf1770fae45fe91e0f25f04ab77c47b950b4
rakibul667s <rakibulislam667s@gmail.com>
2022-11-06 15:33:10 +0600

    forIn

---
+++ 17.forInLoop/main.js
@@ -0,0 +1,21 @@
+
+//forIn loop use in object
+console.log("for object");
+const person ={
+    firstName: "Rakibul",
+    lastName: "Islam Sony",
+    id:62,
+};
+for(let x in person){
+    console.log(person[x]);
+}
+
+
+//forIn loop use in object
+console.log("for array");
+const person2 =[22,33,55,332];
+for(let x in person2){
+    console.log(person2[x]);
+}
+
+//array use is forLoop and best method
