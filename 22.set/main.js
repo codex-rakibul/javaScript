9b2b0ad513ecf3c6420fd3a59c123cc794c7e4f7
rakibul667s <rakibulislam667s@gmail.com>
2022-11-08 14:08:55 +0600

    set

--- 22.set/main.js
+++ 22.set/main.js
@@ -8,3 +8,13 @@ letters.add("c");
 document.getElementById("demo").innerHTML = letters.size;
 
 console.log(letters);
+
+
+// set with foreach
+const letters2 = new Set(["a","b","c"]);
+let text =" ";
+letters2.forEach(function(value) {
+    text += value;
+});
+console.log(text);
+console.log(letters2.values());
